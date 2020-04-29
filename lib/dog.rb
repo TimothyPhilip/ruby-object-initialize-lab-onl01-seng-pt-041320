e51cddc65558bb26c0100fcd9f7241d0fb76c961
Philippe Timothe <philippetimothe@gmail.com>
2020-04-29 15:52:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,20 @@
 class Dog 
   
-  def initialize(name)
+  def initialize(name, breed)
     @name = name 
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def breed=(breed)
+    @breed = breed
   end
   
   
